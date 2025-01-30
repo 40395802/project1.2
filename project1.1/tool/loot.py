bae601b5c773ae814849a9c1da300c7fb01b93c6
40395802 <ysp010107@gmail.com>
2025-01-31 02:47:34 +0900

    Delete project1.1 directory

--- project1.1/tool/loot.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if os.path.exists("dataset/accident"):
-    print("파일이 존재합니다.")
-else:
-    print("파일이 존재하지 않습니다.")
--- project1.1/tool/sound.py
+++
@@ -1,3 +0,0 @@
-import librosa
-y, sr = librosa.load("dataset/accident/2.wav", sr=None)
-print(f"샘플링 레이트: {sr}Hz, 길이: {len(y)/sr:.2f}초")
